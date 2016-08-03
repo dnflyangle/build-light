7ce481e3e372d960ba059fd788f152a14a99df3e
Stephen Pham <spham@thoughtworks.com>
2016-08-03 11:48:50 +1000

    Move firmware jar for siren of shame into device folder and add method to set lights and audio

--- lib/device_recipes/automated_architecture/siren_of_shame.rb
+++ lib/device_recipes/automated_architecture/siren_of_shame.rb
@@ -4,19 +4,19 @@ module Blinky
 
       def success!
         # Ding! sound and fade lights for 10 seconds
-        system( "java -jar soscmd-1.0.jar -a 2 10" )
-        system( "java -jar soscmd-1.0.jar -l 3 10" )
+        set_audio_pattern(2, 10)
+        set_flash_pattern(3, 10)
       end
 
       def failure!
         # Sad trombone sound for 10 seconds and Chase lights
-        system( "java -jar soscmd-1.0.jar -a 1 10" )
-        system( "java -jar soscmd-1.0.jar -l 2 999" )
+        set_audio_pattern(1, 10)
+        set_flash_pattern(4, 999)
       end
 
       def building!
         # On/Off lights
-        system( "java -jar soscmd-1.0.jar -l 2 999" )
+        set_flash_pattern(2, 999)
       end
 
       def warning!
@@ -30,8 +30,16 @@ module Blinky
       end
 
       def stop
-         system( "java -jar soscmd-1.0.jar -sl" )
-         system( "java -jar soscmd-1.0.jar -sa" )
+         system( "java -jar lib/device_recipes/automated_architecture/soscmd-1.0.jar -sl" )
+         system( "java -jar lib/device_recipes/automated_architecture/soscmd-1.0.jar -sa" )
+      end
+
+      def set_flash_pattern (pattern, duration)
+          system( "java -jar lib/device_recipes/automated_architecture/soscmd-1.0.jar -l #{pattern} #{duration}" )
+      end
+
+      def set_audio_pattern (pattern, duration)
+          system( "java -jar lib/device_recipes/automated_architecture/soscmd-1.0.jar -a #{pattern} #{duration}" )
       end
     end
   end
